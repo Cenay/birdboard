5608c7c60fe5469c0c664aac2d1e313406f09efd
Cenay Nailor <cenay@cenaynailor.com>
2020-01-12 20:49:41 -0700

    Convert to the resource method of routing

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,19 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/projects', 'ProjectsController@index');
-    Route::post('/projects', 'ProjectsController@store');
-
-    Route::get('/projects/create', 'ProjectsController@create');
-    Route::get('/projects/{project}', 'ProjectsController@show');
-    Route::get('/projects/{project}/edit', 'ProjectsController@edit');
-    Route::patch('/projects/{project}', 'ProjectsController@update');
+    // Route::get('/projects', 'ProjectsController@index');
+    // Route::post('/projects', 'ProjectsController@store');
+    // Route::get('/projects/create', 'ProjectsController@create');
+    // Route::get('/projects/{project}', 'ProjectsController@show');
+    // Route::get('/projects/{project}/edit', 'ProjectsController@edit');
+    // Route::patch('/projects/{project}', 'ProjectsController@update');
+	// Route::delete('/projects/{project}', 'ProjectsController@destroy');
+	// Refactors down to this: 
+	Route::resource('projects', 'ProjectsController');
+	
     Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
-    Route::patch('/projects/{project}/tasks/{task}', 'ProjectTasksController@update');
+	Route::patch('/projects/{project}/tasks/{task}', 'ProjectTasksController@update');
+	
     Route::get('/home', 'HomeController@index')->name('home');
 });
 
